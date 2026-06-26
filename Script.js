8e8b908d5736b4359751f8c8d45259818b415ac7
shahidzresumeofficial-wq <shahidzresumeofficial@gmail.com>
2026-06-27 00:07:59 +0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,16 @@
+const searchInput = document.getElementById("search");
 
+searchInput.addEventListener("keyup", function () {
+  const value = this.value.toLowerCase();
+  const cards = document.querySelectorAll(".card");
+
+  cards.forEach(card => {
+    const text = card.innerText.toLowerCase();
+
+    if (text.includes(value)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+});
